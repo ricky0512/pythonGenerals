7d82d57a9a93158ef474d2f542add411e685fd71
ricky0512 <1305585+ricky0512@users.noreply.github.com>
2023-10-09 16:14:51 +0800

    Update myRandom.py
    add result format description
    新增結果格式說明

--- myRandom.py
+++ myRandom.py
@@ -3,10 +3,10 @@
 import random
 
 rann = random.randint(0, 19)
-print(rann)
+print(rann)  #整數
 rann = random.randrange(0, 19, 2)
-print(rann)
+print(rann)  #整數
 rann = random.uniform(1.5, 1.9)
-print(rann)
+print(rann)  #小數
 rann = round(random.uniform(1.5, 1.9), 1)
-print(rann)
+print(rann)  #一位小數
