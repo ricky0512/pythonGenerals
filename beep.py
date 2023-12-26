1ae27fe7e3de4a4f113531de1fb587b5751f10a4
ricky0512 <1305585+ricky0512@users.noreply.github.com>
2023-12-26 10:55:30 +0800

    Create beep.py
    beep sound alert
    發出嗶嗶警示聲

---
+++ beep.py
@@ -0,0 +1,8 @@
+import winsound
+# 發出嗶嗶警示聲
+frequency = 500  # Set Frequency To 2500 Hertz
+duration = 3000  # Set Duration To 1000 ms == 1 second
+winsound.Beep(frequency, duration)
+
+# import webbrowser
+# webbrowser.open(r"file:///D:/somewhere/alert.mp3")
